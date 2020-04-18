26762ce0a0fd9e0684c9693c6f2126f040c8ea90
gggcat <61681628+gggcat@users.noreply.github.com>
2020-04-18 12:33:21 +0900

    docker-selenium standalon server

--- selenium-standalone-server/app/test_code.py
+++ selenium-standalone-server/app/test_code.py
@@ -5,12 +5,12 @@ def test_access(driver):
     driver.get('https://www.google.com')
     driver.save_screenshot('test.png')
     print(driver.title)
-    #driver.quit()
+    driver.quit()
 
 if __name__ == '__main__':
     options = {
-        'command_executor': 'http://selenium-hub:4444/wd/hub',
-        'desired_capabilities': DesiredCapabilities.FIREFOX,
+        'command_executor': 'http://chrome:4444/wd/hub',
+        'desired_capabilities': DesiredCapabilities.CHROME,
     }
     with webdriver.Remote(**options) as driver:
         test_access(driver)
